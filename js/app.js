cb78f4a7e1846132d468bda9242bbe188def2304
schmerzmann <artem.hvoshevskiy@mail.ru>
2025-06-30 15:57:55 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function slidesPlugin(activeSlide) {
+    const slides = document.querySelectorAll(".slide")
+
+    slides[activeSlide].classList.add("active")
+
+    slides.forEach(slide => {
+        slide.addEventListener("click", () => {
+            slides.forEach(slide => slide.classList.remove("active"))
+            slide.classList.add("active")
+        })
+    })
+}
+
+slidesPlugin(2)
